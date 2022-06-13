9ddc2c21734ad548f20f0370b0737990581512de
Taras Krupko <taras.krupko07@gmail.com>
2022-06-14 01:03:35 +0300

    added JsonPropetyName attribute to props in Group.cs

--- src/Chat.Api/Models/Groups/Group.cs
+++ src/Chat.Api/Models/Groups/Group.cs
@@ -1,4 +1,5 @@
 ﻿using Chat.Api.Models.Users;
+using System.Text.Json.Serialization;
 
 namespace Chat.Api.Models.Groups
 {
@@ -7,21 +8,28 @@ namespace Chat.Api.Models.Groups
     /// </summary>
     public class Group
     {
+        /// <summary>
+        /// Gets or sets the group identifier.
+        /// </summary>
+        [JsonPropertyName("group_id")]
         public Guid? GroupId { get; set; }
 
         /// <summary>
         /// Gets or sets the name of the group.
         /// </summary>
+        [JsonPropertyName("group_name")]
         public string? GroupName { get; set; }
 
         /// <summary>
         /// Gets or sets the group image.
         /// </summary>
+        [JsonPropertyName("group_image")]
         public byte[]? GroupImage { get; set; }
 
         /// <summary>
         /// Gets or sets the users.
         /// </summary>
+        [JsonPropertyName("users")]
         public List<User> Users { get; set; }
     }
 }
